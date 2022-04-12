3efc5e18e0e63a3b4f736b75fc8fe703b1fa5687
nasirshariff619 <nasirshariff97@gmail.com>
2022-04-12 13:20:54 +0000

    testing gitignore

---
+++ tests/test_back_end.py
@@ -0,0 +1,64 @@
+from flask_testing import TestCase
+
+from application import app, db
+from application.models import Workouts
+
+from flask import url_for
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///test.db", 
+        SECRET_KEY="Test_Secret_Key",
+        WTF_CSRF_ENABLED=False
+        )
+        return app
+
+    def setup(self):
+        db.create_all()
+        Test = Workouts(workout_muscle_group='Test', 
+        duration='Test' 
+        )
+        db.session.add(Test)
+        db.session.commit()
+
+    def teardown(self):
+        db.session.remove()
+        db.drop.all()
+
+class TestViews(TestBase):
+    def test_new_workout_get(self):
+        response = self.client.get(url_for('new_workout'))
+        self.assertEqual(response.status_code, 200)
+
+    
+class TestCreate(TestBase):
+    def test_workouts_add(self):
+        with self.client:
+            response = self.client.post(
+                '/new_workout',
+                data=dict(
+                   workout_muscle_group = "test1",
+                   duration = "test1"
+                ),
+            )
+
+class TestRead(TestBase):
+    def test_workouts_read(self):
+        response = self.client.get(url_for('index'))
+
+class TestDelete(TestBase):
+    def test_workouts_delete(self):
+        response = self.client.get(url_for('delete_workout', id=1))
+
+
+#class TestUpdate(TestBase):
+    #def test_edit_get():
+        #with self.client:
+            #response = self.client.post('/edit/1',
+            #data=dict(
+            #workout_muscle_group='Test1', 
+            #duration='Test1'
+        #),
+        #follow_redirects=True
+        #)
+        #self.assertIn(b'Test1', response.data)
